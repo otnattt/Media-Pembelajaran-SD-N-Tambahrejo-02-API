55d6733398105b2a545b930569cf8e1005a1a785
otnattt <hermawanlistanto15@gmail.com>
2026-07-08 04:49:18 +0700

    fix jumlah soal empty data handling

--- app/Http/Controllers/JumlahSoalController.php
+++ app/Http/Controllers/JumlahSoalController.php
@@ -7,27 +7,39 @@ use App\Models\JumlahSoal;
 
 class JumlahSoalController extends Controller
 {
-    public function show()
-    {
-        return response()->json(
-            JumlahSoal::first()
-        );
-    }
+   public function show()
+{
+    $data = JumlahSoal::first();
+
+    return response()->json([
+        'jml_soal' => $data ? $data->jml_soal : 5
+    ]);
+}
 
     public function update(Request $request)
-    {
-        $request->validate([
-            'jml_soal' => 'required|integer|min:1'
-        ]);
+{
+    $request->validate([
+        'jml_soal' => 'required|integer|min:1'
+    ]);
 
-        $data = JumlahSoal::first();
+    $data = JumlahSoal::first();
+
+    if ($data) {
 
         $data->update([
             'jml_soal' => $request->jml_soal
         ]);
 
-        return response()->json([
-            'message' => 'Berhasil diubah'
+    } else {
+
+        JumlahSoal::create([
+            'jml_soal' => $request->jml_soal
         ]);
+
     }
+
+    return response()->json([
+        'message' => 'Berhasil diubah'
+    ]);
+}
 }
