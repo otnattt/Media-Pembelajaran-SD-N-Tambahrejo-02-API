a6d043a7406193b06b8d03dcfad3da729837c478
otnattt <hermawanlistanto15@gmail.com>
2026-07-08 04:54:47 +0700

    fix jumlah soal empty data handling

--- app/Http/Controllers/JumlahSoalController.php
+++ app/Http/Controllers/JumlahSoalController.php
@@ -7,39 +7,42 @@ use App\Models\JumlahSoal;
 
 class JumlahSoalController extends Controller
 {
-   public function show()
-{
-    $data = JumlahSoal::first();
+    public function show()
+    {
+        $data = JumlahSoal::first();
 
-    return response()->json([
-        'jml_soal' => $data ? $data->jml_soal : 5
-    ]);
-}
-
-    public function update(Request $request)
-{
-    $request->validate([
-        'jml_soal' => 'required|integer|min:1'
-    ]);
-
-    $data = JumlahSoal::first();
-
-    if ($data) {
-
-        $data->update([
-            'jml_soal' => $request->jml_soal
+        return response()->json([
+            'jml_soal' => $data ? $data->jml_soal : 10
         ]);
-
-    } else {
-
-        JumlahSoal::create([
-            'jml_soal' => $request->jml_soal
-        ]);
-
     }
 
-    return response()->json([
-        'message' => 'Berhasil diubah'
-    ]);
-}
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'jml_soal' => 'required|integer|min:1'
+        ]);
+
+        $data = JumlahSoal::first();
+
+        if ($data) {
+
+            $data->update([
+                'jml_soal' => $request->jml_soal
+            ]);
+
+        } else {
+
+            $data = JumlahSoal::create([
+                'jml_soal' => $request->jml_soal
+            ]);
+
+        }
+
+
+        return response()->json([
+            'message' => 'Berhasil diubah',
+            'data' => $data
+        ]);
+    }
 }
